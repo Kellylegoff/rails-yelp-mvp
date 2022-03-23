3b0c556e58e023ceb49e7002b4370c4341be6a6b
kelly@mavana.earth <kelly@mavana.earth>
2022-03-23 16:31:58 +0100

    EXO2 yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian"}
+maido = { name: "maido", address: "rue Gioffredo", category: "japanese"}
+socca = { name: "socca", address: "quelque part à Nice", category: "french"}
+chez_kelly = { name: "Chez Kelly", address: "9 avenue julien, nice", category: "italian"}
+
+
+[ dishoom, pizza_east, maido, socca, chez_kelly].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
